3a09b32df4eb69f83735507ca2e1975d3cd10fc0
dsouzaricardo <dsouza_ricardo@yahoo.com.br>
2026-04-12 21:39:19 -0300

    Update desafio.lua

--- desafio.lua
+++ desafio.lua
@@ -3,6 +3,9 @@
 
 os.execute("chcp 65001") -- Habilitar UTF-8 no terminal
 
+local positionCard = ""
+local userInput = ""
+
 -- Criatrura
 local monsterName = "ZOMBIE"
 local description = "Um monstro meio morto"
@@ -35,28 +38,95 @@ local function getProgressBar(Attribute)
    return result
 end
 
--- Cartão
-print("=============================================================")
-print("|")
-print("| " .. monsterName)
-print("| " .. description)
-print("|")
-print("| Ítem: " .. item)
-print("| Som: " .. Sound)
-print("| Emoj: " .. emoj)
-print("| Horário Favorito: " .. favoriteTime)
-print("| Mutação: " .. mutation)
-print("|")
-print("| Atributos")
-print("|      Ataque:       " .. getProgressBar(attackAttribute))
-print("|      Defesa:       " .. getProgressBar(defenseAttribute))
-print("|      Vida:         " .. getProgressBar(lifeAttribute))
-print("|      Velocidade:   " .. getProgressBar(speedAttribute))
-print("|      Inteligência: " .. getProgressBar(inteligenceAttribute))
-print("|")
-print("==============================================================")
+-- Função que mostra a frente do cartão
+local function viewFrontOfCard()
 
+   os.execute("cls") -- Limpa terminal
 
+   -- Cartão
+   print("=============================================================")
+   print("|")
+   print("| " .. monsterName)
+   print("| " .. description)
+   print("|")
+   print("| Ítem: " .. item)
+   print("| Som: " .. Sound)
+   print("| Emoj: " .. emoj)
+   print("| Horário Favorito: " .. favoriteTime)
+   print("| Mutação: " .. mutation)
+   print("|")
+   print("| Atributos")
+   print("|      Ataque:       " .. getProgressBar(attackAttribute))
+   print("|      Defesa:       " .. getProgressBar(defenseAttribute))
+   print("|      Vida:         " .. getProgressBar(lifeAttribute))
+   print("|      Velocidade:   " .. getProgressBar(speedAttribute))
+   print("|      Inteligência: " .. getProgressBar(inteligenceAttribute))
+   print("|")
+   print("==============================================================")
+
+positionCard = "frente"
+
+end
+
+-- Função que mostra o verso do cartao
+local function viewBackOfCard()
+
+   os.execute("cls") -- Limpa terminal
+
+   local fullChar = "🟩"
+   local result = ""
+   local count = 0
+
+      -- Criando linha de emoj com função while
+      while count <= 20 do 
+        result = result .. fullChar
+        count = count +1
+      end
+
+    count = 0
+
+    print("")
+    print(result)
+    print("")
+    print("")
+    print("                PARABÉNS")
+    print("")
+    print("Você ganhou 5 rodadas grátis no Litle Tiger")
+    print("")
+    print("")
+    print(result)
+    print("")
+
+positionCard = "verso"
+
+end
+
+viewFrontOfCard() -- Chama função mostra frente do cartão
+
+while true do -- Loop infinito
+
+   if positionCard == "frente" then
+     print(" Digite 'v' para visualizar o verso do cartão ou 's' para sair")
+     userInput = io.read() -- Lê entrada digitada pelo usuário
+        if userInput == "v" then
+         viewBackOfCard() -- Chama função que mostra verso do cartão         
+        end
+        if userInput == "s" then
+         break  -- Para execução do loop infinito     
+        end
+   end
+
+   if positionCard == "verso" then
+      print(" Digite 'f' para visualizar a frente do cartão ou 's' para sair")
+      userInput = io.read()
+         if userInput == "f" then
+            viewFrontOfCard()
+         end
+         if userInput == "s" then
+         break         
+        end
+   end
+end
 
 
 
